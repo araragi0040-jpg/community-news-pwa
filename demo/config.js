d3bd2532c342c9546d5405d6cf448eb42c07e5ab
araragi0040-jpg <araragi0040@gmail.com>
2026-04-11 11:14:08 +0900

    Update config.js

--- demo/config.js
+++ demo/config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  GAS_API_URL: "https://script.google.com/macros/s/AKfycbzyL-rkQTcM_GijeBRHFZT3vfsv58xkQIZFAI6PPPC8IFQ50OB62DJrriiWjOLMyZAE/exec",
+  GAS_API_URL: "https://script.google.com/macros/s/AKfycbw7YwgTW-iau9Ep6tkJYeyMfuS3QugXueWF292eFDECxv_3VDphY837DLTxxkZIkfoSxg/exec",
   MODE: "prod",
   READ_ONLY: false,
   DATA_SOURCE: "api"
