691f7485da0949ecb0682f12ea53b4da8bef8d97
araragi0040-jpg <araragi0040@gmail.com>
2026-05-03 00:06:29 +0900

    Update GACHA_APP_URL to remove trailing slash

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
   GAS_API_URL: "https://script.google.com/macros/s/AKfycbzyL-rkQTcM_GijeBRHFZT3vfsv58xkQIZFAI6PPPC8IFQ50OB62DJrriiWjOLMyZAE/exec",
-  GACHA_APP_URL: "https://araragi0040-jpg.github.io/katariba_gacha/",
+  GACHA_APP_URL: "https://araragi0040-jpg.github.io/katariba_gacha",
   MODE: "prod",
   READ_ONLY: false,
   DATA_SOURCE: "api"
