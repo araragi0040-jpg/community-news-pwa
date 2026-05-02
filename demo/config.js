d8a6b7eb989c8504511819159d9c66da52c9333e
araragi0040-jpg <araragi0040@gmail.com>
2026-05-02 20:59:12 +0900

    Update config.js

--- demo/config.js
+++ demo/config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
   GAS_API_URL: "https://script.google.com/macros/s/AKfycbw7YwgTW-iau9Ep6tkJYeyMfuS3QugXueWF292eFDECxv_3VDphY837DLTxxkZIkfoSxg/exec",
-  GACHA_APP_URL: "https://katariba-gacha.vercel.app"
+  GACHA_APP_URL: "https://araragi0040-jpg.github.io/katariba_gacha"
   MODE: "prod",
   READ_ONLY: false,
   DATA_SOURCE: "api"
