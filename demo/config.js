2daa818acda94992509a74946e06681946f021ae
araragi0040-jpg <araragi0040@gmail.com>
2026-05-15 18:49:25 +0900

    Update config.js

--- demo/config.js
+++ demo/config.js
@@ -1,7 +1,7 @@
 window.APP_CONFIG = {
   GAS_API_URL: "https://script.google.com/macros/s/AKfycbw7YwgTW-iau9Ep6tkJYeyMfuS3QugXueWF292eFDECxv_3VDphY837DLTxxkZIkfoSxg/exec",
   GACHA_APP_URL: "https://araragi0040-jpg.github.io/katariba_gacha/demo/",
-  PUSH_VAPID_PUBLIC_KEY: "",
+  PUSH_VAPID_PUBLIC_KEY: "BDd9lRlf2g7rK-42hfBs55vULr-hZ00sCYLHXHoxERrUKksDR_pFH8AFbJR4kibWG-BJkaOKp8RZKZJzVNbQSPk",
   PUSH_WEBHOOK_URL: "",
   MODE: "prod",
   READ_ONLY: false,
