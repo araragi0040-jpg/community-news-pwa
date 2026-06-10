8a91ec4855f8ccfbdb9c3df618a7677158c50847
araragi0040-jpg <araragi0040@gmail.com>
2026-06-10 20:18:08 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 window.APP_CONFIG = {
-  GAS_API_URL: "https://script.google.com/macros/s/AKfycbw7YwgTW-iau9Ep6tkJYeyMfuS3QugXueWF292eFDECxv_3VDphY837DLTxxkZIkfoSxg/exec",
-  GACHA_APP_URL: "https://araragi0040-jpg.github.io/katariba_gacha/demo/",
+  GAS_API_URL: "https://script.google.com/macros/s/AKfycbzyL-rkQTcM_GijeBRHFZT3vfsv58xkQIZFAI6PPPC8IFQ50OB62DJrriiWjOLMyZAE/exec",
+  GACHA_APP_URL: "https://araragi0040-jpg.github.io/katariba_gacha/",
   PUSH_VAPID_PUBLIC_KEY: "BDd9lRlf2g7rK-42hfBs55vULr-hZ00sCYLHXHoxERrUKksDR_pFH8AFbJR4kibWG-BJkaOKp8RZKZJzVNbQSPk",
-  PUSH_WEBHOOK_URL: "https://community-news-pwa-demo.vercel.app/api/push/send",
-  PUSH_SEND_API_URL: "https://community-news-pwa-demo.vercel.app/api/push/send",
+  PUSH_WEBHOOK_URL: "https://community-news-pwa.vercel.app/api/push/send",
+  PUSH_SEND_API_URL: "https://community-news-pwa.vercel.app/api/push/send",
   MODE: "demo",
   READ_ONLY: false,
   DATA_SOURCE: "api"
